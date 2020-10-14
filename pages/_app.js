2043aa47d0364311eb5fe3d4178504dab05547d6
AndiT Service <webguru071@gmail.com>
2020-10-14 17:13:52 +0600

    Ready for Deploy Again

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,10 @@
 import '../styles/globals.css'
 import React from 'react'
+import "bootstrap-css-only/css/bootstrap.min.css"
 
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "../public/assets/css/style.css"
 import "../public/assets/css/color.css"
 import "../public/assets/css/responsive.css"
-import "bootstrap-css-only/css/bootstrap.min.css"
 import'mdbreact/dist/css/mdb.css'
 
 function MyApp({ Component, pageProps }) {
