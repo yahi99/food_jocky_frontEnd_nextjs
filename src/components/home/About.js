e4bd510a5b347089cf8b24bfd3376a60e820c81a
AndiT Service <webguru071@gmail.com>
2020-11-10 14:48:04 +0600

    Modifications

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Link from "next/link";
 
 const About = () => {
-  const AboutData =  { 
+  const AboutData =  {
     heading:"List your Restaurant on FoodJocky",
     para1:"By fulfilling deliveries as far as 25 miles from your location, Foodjocky enables your business to reach thousands of new customers to taste your amazing food? So would we!",
     para2:"Make up to 60% net profit on incremental delivery orders. With none of the overhead costs of a traditional dine-in experience, you can focus on cooking—we'll do the rest.",
     para3:"Interested? Let's start our partnership today!",
     button:"Get started"
   }
- 
+
  return (
   <>
    <section id="about-area">
@@ -22,7 +22,7 @@ const About = () => {
             <p>{AboutData.para2}</p>
             <p>{AboutData.para3}</p>
             <div className="about-food-btn text-right">
-                <Link href="https://dashboard.foodjocky.com">
+                <Link href="https://dashboard.foodjocky.com/register">
                     <a className="btn button-site">{AboutData.button}</a>
                 </Link>
             </div>
