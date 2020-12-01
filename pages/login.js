666a3c23de42856f64c4126c1493793d30153452
AndiT Service <webguru071@gmail.com>
2020-12-01 12:18:34 +0600

    Final Touch

--- pages/login.js
+++ pages/login.js
@@ -3,22 +3,23 @@ import LoginArea from "../src/components/Login/LoginArea";
 import Loader from "../src/components/Common/Loader";
 import Layout from "../src/components/layouts/main";
 
-function login(props) { 
+function login(props) {
     return (
         <Layout>
             <Loader/>
-            <LoginArea apiUrl={props.apiUrl}/>
+            <LoginArea apiUrl={props.apiUrl} referer/>
         </Layout>
-  ); 
+  );
 }
 
-export async function getStaticProps(context) {
-
+export async function getServerSideProps(context) {
+    console.log(context.req.headers.referer)
 
     const apiUrl = process.env.API_URL;
     return {
         props: {
             apiUrl,
+            referer: context.req.headers.referer,
         },
     };
 }
--- src/components/home/PopulerSliderData.js
+++ src/components/home/PopulerSliderData.js
@@ -53,6 +53,124 @@ const PopulerSliderData = [{
     heading: "shawarma",
     title: "39 Restaurants",
     img: shawarma
+  },
+  {
+    heading: "BBQ Chicken",
+    title: "39 Restaurants",
+    img: "/assets/img/category/bbq-chicken.png"
+  },
+  {
+    heading: "Beef Curry",
+    title: "17 Restaurants",
+    img: "/assets/img/category/Beef-Curry.png"
+  },{
+    heading: "Bread",
+    title: "06 Restaurants",
+    img: "/assets/img/category/Bread.png"
+  },{
+    heading: "Break Fast",
+    title: "12 Restaurants",
+    img: "/assets/img/category/Break-fast.png"
+  },{
+    heading: "Chicken Biryani",
+    title: "27 Restaurants",
+    img: "/assets/img/category/Chicken-biryani.png"
+  },{
+    heading: "Chicken Curry",
+    title: "14 Restaurants",
+    img: "/assets/img/category/Chicken-Curry.png"
+  },{
+    heading: "Chinese",
+    title: "21 Restaurants",
+    img: "/assets/img/category/chinese.png"
+  },{
+    heading: "Chotpoti",
+    title: "12 Restaurants",
+    img: "/assets/img/category/chotpoti.png"
+  },{
+    heading: "Coffee",
+    title: "10 Restaurants",
+    img: "/assets/img/category/coffee.png"
+  },{
+    heading: "Desserts",
+    title: "11 Restaurants",
+    img: "/assets/img/category/Desserts.png"
+  },{
+    heading: "Fish Curry",
+    title: "26 Restaurants",
+    img: "/assets/img/category/Fish-Curry.png"
+  },{
+    heading: "Fruits",
+    title: "14 Restaurants",
+    img: "/assets/img/category/Fruits.png"
+  },{
+    heading: "Fuchka",
+    title: "39 Restaurants",
+    img: "/assets/img/category/Fuchka.png"
+  },{
+    heading: "Ice-cream",
+    title: "05 Restaurants",
+    img: "/assets/img/category/ice-cream.png"
+  },{
+    heading: "Juice",
+    title: "17 Restaurants",
+    img: "/assets/img/category/Juice.png"
+  },{
+    heading: "Kebab & Grill",
+    title: "11 Restaurants",
+    img: "/assets/img/category/Kebab-&-Grill.png"
+  },{
+    heading: "Momo",
+    title: "18 Restaurants",
+    img: "/assets/img/category/momo.png"
+  },{
+    heading: "Mutton Biriyani",
+    title: "09 Restaurants",
+    img: "/assets/img/category/mutton-biriyani.png"
+  },{
+    heading: "Mutton Curry",
+    title: "33 Restaurants",
+    img: "/assets/img/category/mutton-curry.png"
+  },{
+    heading: "Noodles",
+    title: "19 Restaurants",
+    img: "/assets/img/category/Noodles.png"
+  },{
+    heading: "Pasta",
+    title: "22 Restaurants",
+    img: "/assets/img/category/Pasta.png"
+  },{
+    heading: "Pitha",
+    title: "03 Restaurants",
+    img: "/assets/img/category/Pitha.png"
+  },{
+    heading: "Salad",
+    title: "10 Restaurants",
+    img: "/assets/img/category/Salad.png"
+  },{
+    heading: "Seafood",
+    title: "03 Restaurants",
+    img: "/assets/img/category/Seafood.png"
+  },{
+    heading: "Shrimp",
+    title: "10 Restaurants",
+    img: "/assets/img/category/shrimp.png"
+  },{
+    heading: "Snacks",
+    title: "15 Restaurants",
+    img: "/assets/img/category/Snacks.png"
+  },{
+    heading: "Soup",
+    title: "20 Restaurants",
+    img: "/assets/img/category/soup.png"
+  },{
+    heading: "Sweets",
+    title: "29 Restaurants",
+    img: "/assets/img/category/Sweets.png"
+  },{
+    heading: "Tea",
+    title: "10 Restaurants",
+    img: "/assets/img/category/Tea.png"
   }
 ];
 
