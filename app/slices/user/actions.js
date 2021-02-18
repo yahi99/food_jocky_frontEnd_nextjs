31ffed90a958d57de97421b50f0236e20f2e030f
Sketch <tuhin1714@cseku.ac.bd>
2021-02-18 15:54:02 +0600

    Imgbb Key Update

--- app/slices/user/actions.js
+++ app/slices/user/actions.js
@@ -286,7 +286,7 @@ export const uploadImage = async (file) => {
     try {
         const data = new FormData()
         data.append('image', file)
-        let url = "https://api.imgbb.com/1/upload?key=dbe026b9378783fd76fb76f8dea82edb";
+        let url = `https://api.imgbb.com/1/upload?key=${process.env.imgbb_key}`;
         const res = await axios.post(url, data, {})
         if (res.data.success) {
             return res.data.data.image.url
