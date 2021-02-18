76018d920a090817e3bc5bd36604e2b44a884935
Sketch <tuhin1714@cseku.ac.bd>
2021-02-18 15:54:02 +0600

    Imgbb Key Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         admin_url: 'https://app.foodjocky.com',
-        base_url: 'https://foodjocky.com'
+        base_url: 'https://foodjocky.com',
+        imgbb_key: 'ecd00bd91ab62d061472c1e7162d5248',
     },
 }
